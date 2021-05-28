4525f1897adcae8d69be0b89b570c3e78c6d7017
tjzhym <73542521+tjzhym@users.noreply.github.com>
2021-05-28 07:45:16 -0400

    Initial commit

---
+++ Algorithms/Python/0001 Two Sum/Two_Sum.py
@@ -0,0 +1,21 @@
+// Problem :    https://leetcode.com/problems/two-sum/
+// Solution:    https://github.com/tjzhym/LeetCode-solution
+// Author :     zhym
+// Date   :     2021-5-28
+
+
+
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        nums_len = len(nums)
+        for i in range(nums_len):
+            for j in range(i+1,nums_len):
+                if (nums[i] + nums[j] == target):
+                    return [i,j]
+                    break
