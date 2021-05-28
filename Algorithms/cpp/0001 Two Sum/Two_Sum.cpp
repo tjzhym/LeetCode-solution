a11cc28a5febdcb543d747f8e693293d39e3024d
tjzhym <73542521+tjzhym@users.noreply.github.com>
2021-05-28 07:45:16 -0400

    Initial commit

---
+++ Algorithms/cpp/0001 Two Sum/Two_Sum.cpp
@@ -0,0 +1,29 @@
+// Problem :    https://leetcode.com/problems/two-sum/
+// Solution:    https://github.com/tjzhym/LeetCode-solution
+// Author :     zhym
+// Date   :     2021-5-28
+
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for (int i = 0; i < nums.size(); i++)
+        {
+            for (int j = i + 1; j < nums.size(); j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    vector <int> vec{ i,j };
+                    return vec;
+                }
+            }
+        }
+        vector <int> vec{};
+        return vec;
+    }
+};
