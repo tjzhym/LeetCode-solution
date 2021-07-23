3da96e4b7b8b002c3388c1a479f25a61a67b19d5
zhym <tjzhym@hotmail.com>
2021-07-23 09:06:43 -0400

    add 0006 py file

---
+++ Algorithms/Python/0006 ZigZag Conversion/ZigZag_Conversion.py
@@ -0,0 +1,29 @@
+// Problem :    https://leetcode.com/problems/zigzag-conversion/
+// Solution:    https://github.com/tjzhym/LeetCode-solution
+// Author :     zhym (tjzhym)
+// Date   :     2021-7-23
+
+
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        
+        if numRows == 1:
+            return s
+        
+        numTable = [''] * numRows
+        ZigZag = False
+        rowIdx = 0
+
+        for letter in s:
+            numTable[rowIdx] += letter
+            if not ZigZag:
+                rowIdx += 1
+                if rowIdx == numRows :
+                    ZigZag = True
+                    rowIdx -= 2
+            else:
+                rowIdx -= 1
+                if rowIdx <= 0:
+                    ZigZag = False
+        return ''.join(numTable)
+        
