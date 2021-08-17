b434dd397d2192138dc47952fc6fe3a491930260
zhym <tjzhym@hotmail.com>
2021-08-17 07:30:57 -0400

    add 01.01 cpp file

---
+++ Algorithms/Cracking the Coding Interview/01.01/01.01.cpp
@@ -0,0 +1,24 @@
+// Problem :    https://leetcode-cn.com/problems/is-unique-lcci/
+// Solution:    https://github.com/tjzhym/LeetCode-solution
+// Author :     zhym (tjzhym)
+// Date   :     2021-8-17
+
+#include <algorithm>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    bool isUnique(string astr) {
+        if (astr.size() < 2) {
+            return true;
+        }
+        sort(astr.begin(), astr.end());
+        for (int index = 1; index < astr.size(); ++index) {
+            if (astr[index] == astr[index - 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
