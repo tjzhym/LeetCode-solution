46609451ec126f2926379deae106bd7ecc189242
zhym <tjzhym@hotmail.com>
2021-09-22 16:38:10 -0400

    add LC 0402 cpp file

---
+++ Algorithms/LC/cpp/0402 Remove K Digits/LC0402.cpp
@@ -0,0 +1,40 @@
+// Problem :    https://leetcode.com/problems/remove-k-digits/
+// Solution:    https://github.com/tjzhym/LeetCode-solution
+// Author :     zhym (tjzhym)
+// Date   :     2021-9-22
+
+
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+        if (num.size() <= k) {
+            return "0";
+        }
+        vector<char> numberVector;
+        for (auto digit : num) {
+            while (!numberVector.empty() && k && numberVector.back() > digit) {
+                numberVector.pop_back();
+                k--;
+            }
+            numberVector.push_back(digit);
+        }
+        for (; k > 0; k--) {
+            numberVector.pop_back();
+        }
+
+        bool isLeadingZero = true;
+        string numberString;
+        for (auto digit : numberVector) {
+            if (digit == '0' && isLeadingZero) {
+                continue;
+            }
+            isLeadingZero = false;
+            numberString += digit;
+        }
+        return numberString.empty() ? "0" : numberString;
+    }
+};
+
