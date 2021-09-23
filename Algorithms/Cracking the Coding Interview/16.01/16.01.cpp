8d627cad8d7e8d079e3c0797f54cbad0b51f6b00
zhym <tjzhym@hotmail.com>
2021-09-23 00:10:01 -0400

    add 16.01 cpp file

---
+++ Algorithms/Cracking the Coding Interview/16.01/16.01.cpp
@@ -0,0 +1,14 @@
+// Problem :    https://leetcode-cn.com/problems/swap-numbers-lcci/
+// Solution:    https://github.com/tjzhym/LeetCode-solution
+// Author :     zhym (tjzhym)
+// Date   :     2021-9-22
+
+
+class Solution {
+public:
+    vector<int> swapNumbers(vector<int>& numbers) {
+        return { numbers[1], numbers[0] };
+    }
+};
+
+// Brute Force
