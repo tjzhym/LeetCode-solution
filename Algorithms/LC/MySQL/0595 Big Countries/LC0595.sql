cc77192a6cf62e6a11d2cde06f602b704cbf2051
zhym <tjzhym@hotmail.com>
2021-10-13 22:33:24 -0400

    add LC 0595 MySQL file

---
+++ Algorithms/LC/MySQL/0595 Big Countries/LC0595.sql
@@ -0,0 +1,31 @@
+// Problem :    https://leetcode.com/problems/jump-game-ii/
+// Solution:    https://github.com/tjzhym/LeetCode-solution
+// Author :     zhym (tjzhym)
+// Date   :     2021-8-9
+
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int target = nums.size() - 1;
+        int currentReach = 0;
+        int maxReach = 0;
+        int step = 0;
+
+        if (nums.size() < 2) {
+            return 0;
+        }
+
+        for (int index = 0; index < target; index++) {
+            maxReach = max(maxReach, index + nums[index]);
+            if (index == currentReach) {
+                step++;
+                currentReach = maxReach;
+            }
+        }
+        return step;
+    }
+};
+
