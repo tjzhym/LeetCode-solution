61befcca128974ec1ca20ecc7abeab42c8fcfe36
zhym <tjzhym@hotmail.com>
2021-10-20 00:30:57 -0400

    add 01.01 v2 cpp file

---
+++ Algorithms/Cracking the Coding Interview/01.01/01.01_v2.cpp
@@ -0,0 +1,27 @@
+// Problem :    https://leetcode-cn.com/problems/is-unique-lcci/
+// Solution:    https://github.com/tjzhym/LeetCode-solution
+// Author :     zhym (tjzhym)
+// Date   :     2021-10-20
+
+
+#include <algorithm>
+#include <string>
+#include <map>
+using namespace std;
+
+
+class Solution {
+public:
+    bool isUnique(string astr) {
+        map<char, int> hashmap;
+        for (auto c : astr) {
+            ++hashmap[c];
+            if (hashmap[c] > 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+// HashMap
