d3e25ccf803e86d66cceb608763ff83ee3e5e9b5
zhym <tjzhym@hotmail.com>
2021-10-21 11:43:16 -0400

    add comment

--- Algorithms/LC/cpp/0206 Reverse Linked List/LC0206.cpp
+++ Algorithms/LC/cpp/0206 Reverse Linked List/LC0206.cpp
@@ -3,31 +3,28 @@
 // Author :     zhym (tjzhym)
 // Date   :     2021-8-22
 
-#include <vector>
-using namespace std;
 
-//Definition for singly-linked list.
- struct ListNode {
-	int val;
-    ListNode *next;
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
 };
 
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        ListNode* pReversedList = NULL;
-        ListNode* pTemp = NULL;
-
+        ListNode* reversedHead = NULL;
+        ListNode* next = NULL;
         while (head) {
-            pTemp = head->next;
-            head->next = pReversedList;
-            pReversedList = head;
-            head = pTemp;
+            next = head->next;
+            head->next = reversedHead;
+            reversedHead = head;
+            head = next;
         }
-        return pReversedList;
+        return reversedHead;
     }
 };
+
+// Traverse
 
