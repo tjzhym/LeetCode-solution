d1c7d5f0b017e154ba835ff00816b84cc8d2f947
zhym <tjzhym@hotmail.com>
2021-10-26 23:58:19 -0400

    add LC 0237 cpp file

---
+++ Algorithms/LC/cpp/0237 Delete Node in a Linked List/LC0237.cpp
@@ -0,0 +1,23 @@
+// Problem :    https://leetcode.com/problems/delete-node-in-a-linked-list/
+// Solution:    https://github.com/tjzhym/LeetCode-solution
+// Author :     zhym (tjzhym)
+// Date   :     2021-10-26
+
+
+using namespace std;
+
+
+// Definition for singly-linked list.
+struct ListNode {
+	int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        node->val = node->next->val;
+        node->next = node->next->next;
+    }
+};
