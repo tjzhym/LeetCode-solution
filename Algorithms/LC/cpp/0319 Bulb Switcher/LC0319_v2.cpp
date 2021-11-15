3e899b507ff6684032ed75c82b0a7d98a15b0cfd
zhym <tjzhym@hotmail.com>
2021-11-14 22:38:37 -0500

    add LC 0319 v2 cpp file

---
+++ Algorithms/LC/cpp/0319 Bulb Switcher/LC0319_v2.cpp
@@ -0,0 +1,18 @@
+// Problem :    https://leetcode.com/problems/bulb-switcher/
+// Solution:    https://github.com/tjzhym/LeetCode-solution
+// Author :     zhym (tjzhym)
+// Date   :     2021-11-14
+
+
+#include <vector>
+using namespace std;
+
+
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        return sqrt(n);
+    }
+};
+
+// Math
