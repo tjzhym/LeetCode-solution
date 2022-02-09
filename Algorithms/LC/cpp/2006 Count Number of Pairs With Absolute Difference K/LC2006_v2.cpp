8eea1299f729f3a40695ae3afed4cad5554085a5
zhym <tjzhym@hotmail.com>
2022-02-09 15:16:02 +0800

    add LC 2006 v2 cpp file

---
+++ Algorithms/LC/cpp/2006 Count Number of Pairs With Absolute Difference K/LC2006_v2.cpp
@@ -0,0 +1,30 @@
+// Problem :    https://leetcode-cn.com/problems/count-number-of-pairs-with-absolute-difference-k/
+// Solution:    https://github.com/tjzhym/LeetCode-solution
+// Author :     zhym (tjzhym)
+// Date   :     2021-2-9
+
+
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+
+class Solution {
+public:
+    int countKDifference(vector<int>& nums, int k) {
+        int count = 0;
+        unordered_map<int, int> numberCount;
+        for (int index = 0; index < nums.size(); ++index) {
+            if (numberCount.count(nums[index] - k)) {
+                count += numberCount[nums[index] - k];
+            }
+            if (numberCount.count(nums[index] + k)) {
+                count += numberCount[nums[index] + k];
+            }
+            numberCount[nums[index]]++;
+        }
+        return count;
+    }
+};
+
+// Hashmap
